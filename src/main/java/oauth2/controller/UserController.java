1ad4d35583c9bbd48aa676716fbee3c19afc33a1
wonseokyoon <01088115030a@gmail.com>
2025-02-13 19:43:14 +0900

    refactor join

--- src/main/java/oauth2/controller/UserController.java
+++ src/main/java/oauth2/controller/UserController.java
@@ -1,13 +1,15 @@
 package oauth2.controller;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import oauth2.dto.UserDTO;
 import oauth2.dto.UserResponse;
 import oauth2.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,8 +21,20 @@ public class UserController {
         return "joinPage";
     }
 
+    @GetMapping("/join2")
+    public String joinPage2(){
+        return "joinPage";
+    }
+
     @PostMapping("/join")
     public ResponseEntity<UserResponse> join(@ModelAttribute UserDTO dto){
+        UserResponse response=userService.join(dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("join2")
+    @ResponseBody
+    public ResponseEntity<UserResponse> join2(UserDTO dto){
         UserResponse response=userService.join(dto);
         return ResponseEntity.ok(response);
     }
