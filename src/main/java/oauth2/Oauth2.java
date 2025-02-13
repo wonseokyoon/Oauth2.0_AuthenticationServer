565562c7d30691f5fdc4925a0d20ce30b9e0b39e
wonseokyoon <01088115030a@gmail.com>
2025-02-13 18:02:09 +0900

    first commit

---
+++ src/main/java/oauth2/Oauth2.java
@@ -0,0 +1,13 @@
+package oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2.class, args);
+    }
+
+}
