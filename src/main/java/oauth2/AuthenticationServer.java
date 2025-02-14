271c51c5e3f8807e01fbd2f317b1adcec714e146
wonseokyoon <01088115030a@gmail.com>
2025-02-14 17:35:20 +0900

    rename application

--- src/main/java/oauth2/AuthenticationServer.java
+++ src/main/java/oauth2/AuthenticationServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResourceServer {
+public class AuthenticationServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(ResourceServer.class, args);
+        SpringApplication.run(AuthenticationServer.class, args);
     }
 
 }
